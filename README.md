6563cbeff8d377cf05568c5abf2c32bb8b6726f9
matteoferla <matteo.ferla@gmail.com>
2019-02-23 13:54:15 +0000

    :pen: better documentation

--- README.md
+++ README.md
@@ -8,22 +8,159 @@ This led me to wonder what the frequency of these names is.
 
 ## Data
 Consequenly I used the [US census data](https://www.ssa.gov/oact/babynames/limits.html) to find out the frequency of names.        
+I use the US census data, because it is a big country, but because they are wierd and call their kids things like Satan.
+
 For the names of Greek heroes, I copy-pasted several Category pages from Wikipedia.
 This is not perfect and required several iterations —say, "Where is Heracles?" or "Where is Hippolyta?".
 It has several contaminants, but they are generally manageable.       
-One major issue is that there are many name variants, especially given that I am searching Ancient Greek names that are translitterated to Latin or English with different schema.
-A case in point is "Hercules", which is the Latin variant of the name "Heracles".
 
+A nasty downside is that by using general categories ("Greek mythology") I don't have a nice way to split names into greek heroes, gods, titans and so forth.
+
+One major issue is that there are many name variants, especially given that I am searching Ancient Greek names that are translitterated to Latin or English with different schema.
+A case in point is "Hercules", which is the Latin variant of the name "Heracles". But in French it's "Hercule" and in Modern Greek it's "Iraklis".
+
+Also, some weirder cases might or might not appear. Let's take the name 'Ariana', literally, a female Aryan person (_sensu_ ancient Indian/Persian nomands, not German blonds).
+As this is a name of a member of a group of people and no hero had an adventure with Aryans &mdash;not even the argonauts&mdash; the word is not in the categories.
+
+A lot of popular names in Modern Greek are spelt differently than the commonly used transliterated Ancient Greek forms used in English. This is great as I care about hipsters naming their kids Persephone and not so much the Greek community, for which I could simply lookup top names in Greece nowadays.
+
+## Top 50
+The top 50 most common Greek hero/god/titan/princess/queen/king names are:
+
+| Name | Count |
+|Jason | 1033820|
+|Helen | 1021394|
+|Diana | 358861|
+|Phyllis | 323240|
+|Dana | 244562|
+|Ida | 186955|
+|Cassandra | 167631|
+|Claudia | 127063|
+|Iris | 80311|
+|Homer | 61034|
+|Penelope | 59152|
+|Griffin | 38703|
+|Paris | 36583|
+|Daphne | 35066|
+|Phoebe | 32518|
+|Asia | 31539|
+|Athena | 31186|
+|Phoenix | 22537|
+|Angelia | 22208|
+|Ulysses | 17249|
+|Rhea | 16397|
+|Orion | 15228|
+|Minerva | 15162|
+|Thalia | 12945|
+|Maia | 12815|
+|Venus | 8998|
+|Adonis | 8898|
+|Vesta | 8167|
+|Hellen | 7556|
+|Kale | 7411|
+|Halie | 7336|
+|Selene | 6611|
+|Memphis | 6254|
+|Meta | 6177|
+|Atlas | 4655|
+|Melia | 4570|
+|Linus | 4389|
+|Echo | 4021|
+|Cleta | 4012|
+|Charis | 3913|
+|Apollo | 3361|
+|Leda | 3318|
+|Ariadne | 3151|
+|Aura | 3140|
+|Titan | 3122|
+|Dione | 2893|
+|Pandora | 2811|
+|Arne | 2796|
+|Ares | 2625|
+|Gorge | 2602|
 
 ## Trends
-Unsurprisingly, the distribution is a power law like curve, with some familiar names utterly dominant.
-X
+Unsurprisingly, the distribution is a power law like curve, with some familiar names utterly dominant ('Jason', 'Helen', etc.).
 
+I have not checked to see if the curve differs from the distribution of the census as a whole, but it looks to be the case and I would expect it to be given that it is safe to describe many of the names as "unique".
 
 ### Name length
-Excessively long names such as Halirrhothius are absent. Curiously, the longest names at 15 and 14 letters
-XXX
+Excessively long names such as Halirrhothius are absent. Curiously, the longest names are only at 15 and 14 letters, but are monsters.
+The longest heroic names in the census are three 10 letter ones.
 
 * Persephone
 * Cassiopeia
 * Prometheus
+
+Unfortunately, I haven't installed Matlab on my home machine so I cannot do stats. But here is the distribution of letters for men:
+![distro](distribution.png)
+Where 
+
+* `greeks` is the distribution of letter counts of the set of unique greek names in use.
+* `greek_freq` is the distribution of the letter counts weighted by census frequency
+* `census` all unique names
+* `census_freq` weighted by frequency
+
+Basically, there 'appears' to be a Jason/Phoenix effect pumping up the 5 and 7 letter names, but unlike the census as a whole,
+there is a strong bias towards shorter names when frequency is taken into account.
+
+This is less pronounced in women, but also present.
+![women](longer_women.png)
+
+### Heroic power couples
+Jason is a complete arsehole to Medea &mdash;albeit only in the sequel 'Medea', not in 'Argonautica'&mdash; yet he is way more popular.
+Orion is a villain, yet he is a top 50. It is safe to say that Greek names get chosen based on sound rather than origin.
+
+This does not hold true for all...
+* Jason 1,028,809 vs. Medea 202
+* Perseus 405 vs. Andromeda 840
+* Theseus 233 vs. Ariadne 3151
+* Heracles 0 (1168 for Hercules) vs. lots of wives and lovers... all 0 &mdash; including Hippolyta!
+* Orion 15228 vs. Merope 0
+* Atalanta 5 vs. Hippomenes 0
+* Bellerophon 0 vs. Philonoe 0
+* Cadmus 0 vs. Harmonia 0 &mdash;the founder of Thebes gives his name to cadmium, so that is a buzz kill, and Harmonia is the Latin for Harmony...
+* Icarus 54 vs. Daedalus 7
+
+### Trojan war
+This is not too surprising, except for the lower ones.      
+Paris is a _son_ of Priam, but due to the city of Paris, it's a girl's name now in English.         
+I am surprised by Aeneas: he is known as Enea in Italian, but that's not a normal name.    
+Diomedes makes me happy: he's a solid chap and deserves better recognition.     
+Unfortunately, it the census data there is no way of known whether there are an Achilles that are sons of Zeus, which would make Prometheus's prophesy rather interesting (Achilles was fortold to be stronger than his father: liberated Prometheus stopped Zeus from visiting Thesis, which meant he was son of Peleus instead in Homer's Illiad. In Dan Simmon's Illium he is son of Zeus and sends him to Tartarus)  
+
+* Helen	1021394
+* Cassandra	167631
+* Hector	102006
+* Paris	36583
+* Phoenix	22537
+* Nestor	9664
+* Achilles	2320
+* Briseis	1026
+* Aeneas	355
+* Odysseus	224
+* Ajax	214
+* Diomedes	21
+* Priam	13
+* Acamas  11
+
+## Twelve olympians
+Of the 13+1 twelve olympians, there is a curious happening with the Latin and Greek versions. Where it not for Diana, the Greeks would win!
+
+| Zeus | 1,327.0 | Jupiter | 412.0  | 
+| Hera | 493.0 | Juno | 837.0  | 
+| Poseidon | 133.0 | Neptune | -    | 
+| Hades | 48.0 | Pluto | -    | 
+| Demeter | 5.0 | Ceres | 29.0  | 
+| Athena | 31,186.0 | Minerva | 15,162.0  | 
+| Apollo | 3,361.0 	| Apollo | 3,361.0  | 
+| Artemis | 1,357.0 | Diana | 358,861.0  | 
+| Ares | 2,625.0 | Mars | 425.0  | 
+| Aphrodite | 579.0 | Venus | 8,998.0  | 
+| Hermes | 498.0 | Mercury | 139.0  | 
+| Hephaestus | -   | Vulcan | -    | 
+| Hestia | 6.0 | Vesta | 8,167.0  | 
+| Dionysus | 32.0 | Bacchus | -  |
+ 
+
+
--- greek_names.py
+++ greek_names.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 __description__ = \
     """
-A small script to find 
+A small script to find greek names.
+See class HeroicNames for more.
 NB. Written for python 3, not tested under 2.
 """
 __author__ = "Matteo Ferla. [Github](https://github.com/matteoferla)"
@@ -15,82 +16,148 @@ import urllib.request
 import pickle, os, re, csv
 from collections import Counter
 
-#https://www.ssa.gov/oact/babynames/limits.html
+import sys
+from warnings import warn
 
-def iternames():
-    for file in os.listdir('names'):
-        if file.find('yob') == -1:
-            continue
-        year=int(re.search('(\d+)',file).group(1))
-        print(year)
-        for row in open(os.path.join('names',file)):
-            if not row:
-                continue
-            (name,gender,freq)=row.rstrip().split(',')
-            yield (year,name,gender,freq)
+if sys.version_info[0] < 3:
+    warn("Sorry man, I told you to use Python 3.")
 
-def get_heroes(filename):
-    greeks = set()
-    for row in open(filename, encoding="utf-8"):
+
+##############################################################################
+
+class HeroicNames:
+    """
+    Make intance with nothing, iterable or file of greek names. can be messy. But one greek per line.
+    The attribute `.greeks` is the set of names.
+    The iterator iternames() runs through the files. Really fast surprisingly.
+    While iterating through an intsnace of this class runs thorugh the greeks set.
+    parse() parses the census for the greeks stored and makes the.census attribute. This ought to be a class, but shmeh
+    key = hero: value = {1880: {'M': n, 'F': f, 'T': t}, ..., 'total': {'M':m, ...}}
+    To add a greek afterwards use `.add_name(name)` not .`greeks.add(name)` due to the .census
+
+    """
+    groups = ('greeks','greeks_freq','census', 'census_freq')
+    genders = ('T', 'M', 'F')
+
+    def __init__(self, iterable=None, filename=None):
+        """
+        Initialises the name holding instance. `.greeks` is the set.
+        :param iterable: opt.
+        :param filename: opt.
+        """
+        self.greeks = set()
+        self.census = {}  # key = hero: value = {year: {'M': n, 'F': f}} #not a default dictionary due to the tallies.
+        if filename:
+            with open(filename, encoding="utf-8") as w:
+                for row in w:
+                    self.add_name(row)
+        if iterable:
+            for row in iterable:
+                self.add_name(row)
+
+    def __iter__(self):
+        return self.greeks.__iter__()
+
+    def add_name(self, row):
+        """
+        cleans and saves the anme
+        :param row: No name should have spaces or unicode.
+        :return:
+        """
         srow = row.rstrip().replace('Äé', ' ').replace(',', ' ').replace(' ', ' ').split()
         if not srow:
-            continue
+            return self
         name = srow[0].title().replace('ä', 'a').replace('ö', 'o').replace('ë', 'e').replace('ü', 'u')
         if len(name) < 2:
-            continue
-        greeks.add(name)
-    return greeks
+            return self
+        self.greeks.add(name)
+        # fill.census.
+        if name not in self.census:
+            self.census[name] = {year: {'M': 0, 'F': 0, 'T': 0} for year in range(1880, 2018)}
+            self.census[name]['total'] = {'M': 0, 'F': 0, 'T': 0}
+        return self
 
-def find_heroes(filename):
-    # prep
-    greeks=get_heroes(filename)
-    yeardex={hero: {year: 0 for year in range(1880,2018)} for hero in greeks}
-    genderdex={hero: {'M': 0,'F': 0} for hero in greeks}
-    ## parse all data
-    for (year,name,gender,freq) in iternames():
-        if name in greeks:
-            genderdex[name][gender]+=int(freq)
-            yeardex[name][year]+=int(freq) # combine genders
-    # write table
-    with open(filename.replace('.txt','.csv'),'w', newline='') as w:
-        writer=csv.DictWriter(w,['Hero','M','F','T']+list(range(1880,2018)))
-        writer.writeheader()
-        for hero in greeks:
-            writer.writerow({'Hero': hero,
-                             'M': genderdex[hero]['M'],
-                             'F': genderdex[hero]['F'],
-                             'T': genderdex[hero]['M'] + genderdex[hero]['F'],
-                             **yeardex[hero]
-                             })
+    @staticmethod
+    def iternames():
+        """iterates across census not greeks.
+        https://www.ssa.gov/oact/babynames/limits.html"""
+        for file in os.listdir('names'):
+            if file.find('yob') == -1:
+                continue
+            year = int(re.search('(\d+)', file).group(1))
+            print(year)
+            with open(os.path.join('names', file)) as fh:
+                for row in fh:
+                    if not row:
+                        continue
+                    (name, gender, freq) = row.rstrip().split(',')
+                    yield (year, name, gender, freq)
 
-def get_heroic_distribution(greeks):
-    lettercount={'M':Counter(),'F':Counter()}
-    for (year, name, gender, freq) in iternames():
-        if name in greeks:
-            lettercount[gender].update([len(name)])
-    return lettercount
+    def _count(self, name, year, gender, freq=0):
+        # adds to the count
+        amount = int(freq)
+        self.census[name][year][gender] = amount
+        self.census[name][year]['T'] += amount
+        self.census[name]['total'][gender] += amount
+        self.census[name]['total']['T'] += amount
+        return self
+
+    def parse(self):
+        """
+        looks for the greeks in the census and adds them to .census
+        :return:
+        """
+        for (year, name, gender, freq) in self.iternames():
+            if name in self.greeks:
+                self._count(name, year, gender, freq)
+        return self
+
+    def write(self, filename):
+        with open(filename, 'w', newline='') as w:
+            writer = csv.DictWriter(w, ['Hero', 'grantotal', 'male_total', 'female_total'] +
+                                    ['{g}_{i}'.format(g=g, i=i) for g in self.genders for i in range(1880, 2018)])
+            writer.writeheader()
+            for hero in self:
+                writer.writerow({'Hero': hero,
+                                 'male_total': self.census[hero]['total']['M'],
+                                 'female_total': self.census[hero]['total']['F'],
+                                 'grantotal': self.census[hero]['total']['T'],
+                                 **{'{g}_{i}'.format(g=g, i=i):
+                                        self.census[hero][i][g] for g in self.genders for i in range(1880, 2018)}
+                                 })
+        return self
+
+    def get_lettercount(self):
+        # groups ('greeks','greeks_freq','census', 'census_freq')
+        lettercount = {group: {'M': Counter(), 'F': Counter(), 'T': Counter()} for group in self.groups}
+        for (year, name, gender, freq) in self.iternames():
+            lettercount['census'][gender].update([len(name)])
+            lettercount['census_freq'][gender][len(name)] += int(freq)
+            lettercount['census_freq']['T'][len(name)] += int(freq)
+            lettercount['census']['T'].update([len(name)]) #this value is wrong. As the same name as name and female gets counted.
+            if name in self:
+                lettercount['greeks'][gender].update([len(name)])
+                lettercount['greeks_freq'][gender][len(name)] += int(freq)
+                lettercount['census_freq']['T'][len(name)] += int(freq)
+                lettercount['census']['T'].update([len(name)]) #issue.
+        return lettercount
 
 
-def get_distribution():
-    lettercount={'M':Counter(),'F':Counter()}
-    for (year, name, gender, freq) in iternames():
-        lettercount[gender].update([len(name)])
-    return lettercount
-
-def print_distribution(lettercount):
-    print('N', 'M','F')
-    for i in range(1, 20):
-        print(i, lettercount['M'][i], lettercount['F'][i])
+    @classmethod
+    def write_lettercount(cls, lettercount, filename):
+        with open(filename, 'w',newline='') as w:
+            d=csv.DictWriter(w, fieldnames=['N']+[group+'_'+gen for gen in cls.genders for group in cls.groups])
+            d.writeheader()
+            for i in range(1, 20):
+                d.writerow({'N':i,
+                            **{group + '_' + gen: lettercount[group][gen][i] for gen in cls.genders for group in cls.groups}
+                })
+        return cls
 
 
 if __name__ == '__main__':
-    #print_distribution(get_distribution())
-    greeks=get_heroes('greeks.txt')
-    print_distribution(get_heroic_distribution(greeks))
-    #find_heroes('iliad.txt')
-
-
-
-
-
-
+    print('General distribution')
+    hero = HeroicNames(filename='greeks.txt').parse().write('greeks.csv')
+    print('Lettercount')
+    l = hero.get_lettercount() #get is a getter...
+    HeroicNames.write_lettercount(l,'greek_distro.csv')
